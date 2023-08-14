70d4734d151c78e60213f80150803a459a685ae1
Igor Vidal Meneghini <igormeneghini@outlook.com>
2023-08-14 16:31:56 -0300

    update

--- Film.java
+++ Film.java
@@ -1,3 +1,13 @@
+import java.sql.Date;
+
 public class Film {
     int id;
+    String type;
+    String title;
+    String director;
+    String cast;
+    String country;
+    Date dateAdded;
+    String rating;
+    String description;
 }
