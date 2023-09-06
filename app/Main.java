e1ed29c129e41f26e8725746ca8c78b57d69c021
IgorMeneghini <igormeneghini@outlook.com>
2023-09-06 15:47:19 -0300

    update

--- app/Main.java
+++ app/Main.java
@@ -15,7 +15,7 @@ class Main {
         Film film = new Film(8449, "Igor O criador", "Movie", 9999999, "R,90 min");
         crud.update(film);
         System.out.println(crud.delete(8449));
-        System.out.println(crud.sequencialSearch(8449));
+        System.out.println(crud.sequencialSearch(8450));
 
     }
 }
--- archive/ExternalSorting.java
+++ archive/ExternalSorting.java
@@ -9,16 +9,42 @@ public class ExternalSorting {
     private static char tombstone = '¬';
     private static RandomAccessFile raf;
     private static long posOfFilmReading;
+    private static final String dbFilePath = "DataBase/films.db";
 
     ExternalSorting() {
         try {
-            raf = new RandomAccessFile("DataBase/films.db", "rw");
+            raf = new RandomAccessFile(dbFilePath, "rw");
             posOfFilmReading = 4;
         } catch (Exception e) {
         }
     }
 
+    private static void swap(Film[] array, int i, int j) {
+        Film temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
     void quickSort(Film[] array, int left, int right) {
+        int i = left, j = right;
+        Film pivot = array[(right + left) / 2];
+        while (i <= j) {
+            while (array[i].getTitle().compareTo(pivot.getTitle()) < 0)
+                i++;
+            while (array[j].getTitle().compareTo(pivot.getTitle()) > 0)
+                j++;
+            if (i <= j) {
+                swap(array, i, j);
+                i++;
+                j++;
+            }
+        }
+        if (left < j) {
+            quickSort(array, left, j);
+        }
+        if (right > i) {
+            quickSort(array, i, right);
+        }
     }
 
     Film readFilm() throws IOException {
@@ -31,8 +57,8 @@ public class ExternalSorting {
             while (film.getTombstone() != tombstone) {
                 int registerSize = raf.readInt();
                 raf.skipBytes(registerSize);
-                film.setTombstone(raf.readChar());
             }
+            film.setTombstone(raf.readChar());
             int registerSize = raf.readInt();
             byte[] filmData = new byte[registerSize];
             raf.read(filmData);
@@ -42,8 +68,21 @@ public class ExternalSorting {
         return null;
     }
 
-    void balance(int B, int M) throws IOException {
-
+    void balance(int numberOfBlocks, int memorySize) throws IOException {
+        RandomAccessFile[] tempFiles = new RandomAccessFile[numberOfBlocks];
+        for (int i = 0; i < numberOfBlocks - 1; i++) {
+            tempFiles[i] = new RandomAccessFile("outputs/tempFile_" + i + ".db", "rw");
+        }
+        for (int i = 0; i < numberOfBlocks; i++) {
+            Film[] buffer = new Film[memorySize];
+            for (int j = 0; j < memorySize; j++) {
+                Film film = readFilm();
+                if (film == null)
+                    break;
+                buffer[j] = film;
+            }
+            quickSort(buffer, 0, memorySize - 1);
+        }
     }
 
 }
